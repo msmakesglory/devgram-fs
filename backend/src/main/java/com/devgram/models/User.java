037da9b4cef4b510837cdfebded25819ca726470
Manoj <rayimanoj8@gmail.com>
2025-03-26 12:55:49 +0530

    db props

--- backend/src/main/java/com/devgram/models/User.java
+++ backend/src/main/java/com/devgram/models/User.java
@@ -1,8 +1,15 @@
 package com.devgram.models;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
-
+    @Id
+    private String userId;
+    private String email;
+    private String profilePicture;
+    private String fullName;
+    private String username;
 }
